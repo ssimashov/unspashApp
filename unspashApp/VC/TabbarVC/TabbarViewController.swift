e9d1323c733c4db138c602015d4b998dd4a1bce5
Sergey Simashov <simashov@gmail.com>
2022-09-02 22:24:17 +0300

    added initial tabor vc

---
+++ unspashApp/VC/TabbarVC/TabbarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TabbarViewController.swift
+//  unspashApp
+//
+//  Created by Sergey Simashov on 02.09.2022.
+//
+
+import UIKit
+
+class TabbarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
