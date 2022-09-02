2bbfc5e1c9aa3abed9c121a9e475094dbaae82bd
Sergey Simashov <simashov@gmail.com>
2022-09-02 21:26:50 +0300

    Initial Commit

---
+++ unspashApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  unspashApp
+//
+//  Created by Sergey Simashov on 02.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
